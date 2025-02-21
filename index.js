63b6318af0aea9636e3f8df8886fe1b2b74261ad
Jollypru <Jollyprumarma2020@gmail.com>
2025-02-22 02:20:40 +0600

    update functionality

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nor5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +48,15 @@ async function run() {
 
         const result = await taskCollection.insertOne(newTask);
         res.json({ success: true, task: { ...newTask, _id: result.insertedId } });
+    })
+
+    app.put('/tasks/:id', async(req, res) => {
+        const {id} = req.params;
+        const {title, description,category} = req.body;
+        const result = await taskCollection.updateOne({_id: new ObjectId(id)}, {
+            $set: {title, description,category}
+        });
+        res.json({success: true})
     })
 
 
