22b3eb055a0d215dd039d41c7058ea49eebe05f2
Jollypru <Jollyprumarma2020@gmail.com>
2025-02-22 19:28:47 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const WebSocket = require("ws");
 const app = express();
 const port = process.env.PORT || 5000 ;
 
@@ -9,9 +11,8 @@ app.use(cors({
     origin: ['http://localhost:5175','https://task-manager-client-two.vercel.app', 'https://task-manager-client-ptdh5dkwz-jolly181s-projects.vercel.app']
 }));
 
+const server = http.createServer(app);
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const WebSocket = require("ws");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nor5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,7 +31,6 @@ async function run() {
     const userCollection = client.db('TaskManagerDB').collection('users');
     const taskCollection = client.db('TaskManagerDB').collection('tasks');
 
-    const server = require('http').createServer(app);
     const wss = new WebSocket.Server({server});
     wss.on("connection", (ws) => {
         // console.log('Client connected to websocket');
@@ -104,6 +104,10 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    server.listen(port, () => {
+        console.log(`server is running on port: ${port}`);
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -112,6 +116,3 @@ async function run() {
 run().catch(console.dir);
 
 
-server.listen(port, () => {
-    console.log(`server is running on port: ${port}`);
-})
